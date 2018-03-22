3e1b01c621ee750f8f223ee4d92ff082576ce77b
Jose Perez <josegperez@mail.com>
2018-03-21 23:58:34 -0600

    SRS 20

--- field.py
+++ field.py
@@ -92,7 +92,7 @@ class EndField(Table):
     def __init__(self, parent):
         Table.__init__(self, parent)
 
-class ReferenceList(Table):
+class ReferenceListField(Table):
     def __init__(self, parent):
         Table.__init__(self, parent)
         self.init_components()
@@ -100,11 +100,15 @@ class ReferenceList(Table):
         self.references = []
         self.add_reference_slot()
 
+    def set_title(self, title):
+        self.title("Reference List [" + title + "]")
+
     def init_components(self):
-        self.add_entry('Reference List Name', width=25)
+        self.set_title('Reference List Name')
+        self.add_entry('Reference List Name', width=45, command=self.on_referencename_change)
 
         self.lbl_value = tk.Label(self, text='Value')
-        self.lbl_desc = tk.Label(self, text='Description')
+        self.lbl_desc = tk.Label(self, text='Text Description')
         self.lbl_value.grid(column=0, row=self.row)
         self.lbl_desc.grid(column=1, row=self.row)
         self.row += 1
@@ -120,10 +124,55 @@ class ReferenceList(Table):
         else:
             self.add_reference_slot()
 
+    def on_referencename_change(self):
+        new_title = self.components['Reference List Name'].get()
+        self.set_title(new_title)
+
     def add_reference_slot(self):
         var_value = tk.StringVar()
         desc_value = tk.StringVar()
         self.references.append((var_value, desc_value))
+
+        entry_value = tk.Entry(self, textvariable=var_value, width=15)
+        entry_desc = tk.Entry(self, textvariable=desc_value, width=50)
+
+        entry_value.grid(column=0, row=self.row)
+        entry_desc.grid(column=1, row=self.row)
+        self.row += 1
+
+        self.btn_add.grid(column=1, row=self.row+1)
+
+class PacketInformationField(Table):
+    def __init__(self, parent):
+        Table.__init__(self, parent)
+        self.init_components()
+
+        self.info = []
+        self.add_info_slot()
+
+    def init_components(self):
+        self.title('Packet Information')
+        self.lbl_value = tk.Label(self, text='Value')
+        self.lbl_desc = tk.Label(self, text='Text Description')
+        self.lbl_value.grid(column=0, row=self.row)
+        self.lbl_desc.grid(column=1, row=self.row)
+        self.row += 1
+
+        self.img_btn = ImageTk.PhotoImage(Image.open('plus-symbol-round-button.png'))
+        self.btn_add = tk.Button(self, image=self.img_btn, command=self.on_btn_add)
+        self.btn_add.grid(column=1, row=self.row + 1)
+
+    def on_btn_add(self):
+        (var_value, var_desc) = self.info[-1]
+        if not var_value.get() or not var_desc.get():
+            pass
+        else:
+            self.add_info_slot()
+
+    def add_info_slot(self):
+        var_value = tk.StringVar()
+        desc_value = tk.StringVar()
+        self.info.append((var_value, desc_value))
 
         entry_value = tk.Entry(self, textvariable=var_value)
         entry_desc = tk.Entry(self, textvariable=desc_value)
@@ -137,5 +186,6 @@ class ReferenceList(Table):
 if __name__ == '__main__':
     # form = FieldDialog(None)
     # form = StartField(None)
-    form = ReferenceList(None)
+    form = ReferenceListField(None)
+    # form = PacketInformationField(None)
     form.mainloop()
